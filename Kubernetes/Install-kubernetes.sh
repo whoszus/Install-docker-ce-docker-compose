e6a2cdb5dba4b7e4373e8df84d74b16670cfd40f
tinker.siat <whosuzs@yahoo.com>
2020-11-13 11:11:37 +0800

    update

--- Kubernetes/Install-kubernetes.sh
+++ Kubernetes/Install-kubernetes.sh
@@ -1,11 +1,3 @@
-
-#edit nameserver and host
-sudo -s echo "tinker.siat.ac.cn" >> /etc/hosts;
-sudo echo "nameserver 223.5.5.5" > /etc/resolvconf/resolv.conf.d/base;
-
-# get installation packages from file-server
-
-cd /home && wget http://tinker.siat.ac.cn/files/shares/intall-kubernetes.tar.gz;
-cd /home && tar -zxvf install-kubernetes.tar.gz;
-echo 'root:@@tinker' | chpasswd && echo "@@tinker" |su root;
-cd /home/install-kubernetes;
+sudo wget http://172.169.8.254:10000/files/shares/install-kubernetes-worker.tar.gz &&\
+sudo tar -zxvf install-kubernetes-worker.tar.gz && cd install-kubernetes-worker/kube_pack && \
+./worker_node.sh;
